6192aa04897d472a9c70e83706d03c93cf59ff4c
Vilém Charwot <vilem.charwot@proton.me>
2026-05-09 16:16:15 +0200

    Edit compile/clean scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Smazání souborů dle seznamu
-for pattern in "*.aux" "*.log" "*.out" "*.toc" "*.lof" "*.lot" "*.bbl" "*.blg" "*.synctex.gz" "*.fls" "*.fdb_latexmk" "*.xmpi" "*.txss2"; do
-    find . -type f -name "$pattern" -exec rm -f {} +
-done
+latexmk -c
 
 echo "Nepotřebné soubory byly smazány."
--- compile.sh
+++ compile.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # Název souboru bez přípony
 filename="prace"
 
-latexmk -c 
 latexmk -pdf "$filename.tex"
 
 echo "Kompilace dokončena."
